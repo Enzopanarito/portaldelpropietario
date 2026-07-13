aa8b74037318affb069e114f25d8c0201e10b1b5
Enzopanarito <enzopanarito@gmail.com>
2026-07-13 03:35:20 -0400

    Añadir configuración runtime segura por defecto

---
+++ netlify/functions/_runtime_config.generated.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Este archivo se regenera durante cada build de Netlify.
+// El valor versionado es deliberadamente seguro para desarrollo y CI.
+module.exports=Object.freeze({
+ schemaVersion:1,
+ generated:false,
+ deployContext:'local',
+ publicBlobCacheEnabled:false,
+ publicBlobCacheMaxAgeMs:120000,
+ dataEnvironment:'legacy'
+});
