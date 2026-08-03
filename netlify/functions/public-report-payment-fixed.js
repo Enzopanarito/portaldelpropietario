7971af81ae3f63feb4867644490f66efb908c89c
Enzopanarito <enzopanarito@gmail.com>
2026-08-03 06:08:30 -0400

    Mostrar causa real cuando falle el reporte de pago

--- netlify/functions/public-report-payment-fixed.js
+++ netlify/functions/public-report-payment-fixed.js
@@ -7,4 +7,17 @@ const compatibleProofStore = require('./_payment_proof_store_compat');
 // su implementación de almacenamiento por una capa compatible con la API actual.
 require.cache[proofStorePath].exports = compatibleProofStore;
 
-module.exports = require('./public-report-payment');
+const original = require('./public-report-payment');
+
+exports.handler = async function handler(event, context) {
+  const response = await original.handler(event, context);
+  if (!response || Number(response.statusCode || 0) < 400) return response;
+
+  try {
+    const body = JSON.parse(response.body || '{}');
+    if (body.detail) body.message = body.detail;
+    response.body = JSON.stringify(body);
+  } catch (_) {}
+
+  return response;
+};
