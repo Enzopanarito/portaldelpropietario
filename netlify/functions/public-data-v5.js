7c1ae3fc2dd2b434ddafed651d2d395bfd66e9f8
Enzopanarito <enzopanarito@gmail.com>
2026-09-01 10:40:50 -0400

    Eliminar endpoint público auxiliar innecesario

--- netlify/functions/public-data-v5.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const snapshotLayer=require('./public-data-v3');
-const guardedAccounting=require('./public-data-v4');
-
-const handler=snapshotLayer.createHandler({previousHandler:guardedAccounting.handler});
-exports.handler=handler;
-module.exports={handler};
