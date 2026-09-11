22d18a97b643b61eb6eff3e17f9a97f4895351ec
Enzopanarito <enzopanarito@gmail.com>
2026-09-10 23:29:20 -0400

    Use native ESM Blobs client and require real deployed storage check

--- netlify/functions/admin-communications.mjs
+++ netlify/functions/admin-communications.mjs
@@ -1,6 +1,9 @@
-import '@netlify/blobs';
+import { getStore } from '@netlify/blobs';
+import store from './_shared/_communications_store.js';
 import legacy from './_shared/_communications_admin_handler.js';
 import { invokeLegacy } from './_shared/legacy-function-bridge.mjs';
+
+store.configureNativeStore(getStore);
 
 export default (request, context) => invokeLegacy(request, context, legacy.handler);
 
--- netlify/functions/communications-dispatch-background.mjs
+++ netlify/functions/communications-dispatch-background.mjs
@@ -1,7 +1,10 @@
-import '@netlify/blobs';
+import { getStore } from '@netlify/blobs';
+import store from './_shared/_communications_store.js';
 import 'nodemailer';
 import legacy from './_shared/_communications_dispatch_handler.js';
 import { invokeLegacy } from './_shared/legacy-function-bridge.mjs';
+
+store.configureNativeStore(getStore);
 
 export default (request, context) => invokeLegacy(request, context, legacy.handler);
 
